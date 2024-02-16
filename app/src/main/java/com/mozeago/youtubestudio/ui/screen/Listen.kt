4897c67bfbace3ef673c0796713549da80f2a75c
Moses Asiago <mozzeago@gmail.com>
2024-02-16 23:16:17 +0300

    added Compose Navigation dependency, configured screen navigation and switch,

---
+++ app/src/main/java/com/mozeago/youtubestudio/ui/screen/Listen.kt
@@ -0,0 +1,22 @@
+package com.mozeago.youtubestudio.ui.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Listen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.padding(50.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = "Listen", style = MaterialTheme.typography.displayLarge)
+    }
+}
